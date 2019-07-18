ca19f87d7e5191538f560118ee12785933f37824
ELBEQQAL94 <keep.try77@gmail.com>
2019-07-18 10:23:10 +0000

    isArray method added

---
+++ Array/isArray.js
@@ -0,0 +1,37 @@
+// isArray() method check if is an array or not
+// some examples
+
+Array.isArray([1, 2, 3]);  // true
+Array.isArray({foo: 123}); // false
+Array.isArray('foobar');   // false
+Array.isArray(undefined);  // false
+
+// isArray(value)
+// value: the value to be checked
+
+// return true if the value is an array otherwise false.
+
+// more examples
+
+// all following calls return true
+Array.isArray([]);
+Array.isArray([1]);
+Array.isArray(new Array());
+Array.isArray(new Array('a', 'b', 'c', 'd'));
+Array.isArray(new Array(3));
+// Little known fact: Array.prototype itself is an array:
+Array.isArray(Array.prototype); 
+
+// all following calls return false
+Array.isArray();
+Array.isArray({});
+Array.isArray(null);
+Array.isArray(undefined);
+Array.isArray(17);
+Array.isArray('Array');
+Array.isArray(true);
+Array.isArray(false);
+Array.isArray(new Uint8Array(32));
+Array.isArray({ __proto__: Array.prototype });
+
+// resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
